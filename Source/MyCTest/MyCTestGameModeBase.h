48967147bd120374872b5403fe83df7c321150af
influence-wp <3608638156@qq.com>
2022-03-04 22:10:20 +0800

    添加过滤条件

---
+++ Source/MyCTest/MyCTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyCTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYCTEST_API AMyCTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
